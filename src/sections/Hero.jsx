2fa124c9edd79ce2e57bcfdebdf2db0b263a7d4a
Carlos <same.carlosartiles@gmail.com>
2026-09-25 16:01:46 -0400

    changed the text for software engineer

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section id="hero" className="section">
       <h1>Carlos Artiles</h1>
-      <p className="subtitle">Aspiring Software Engineer</p>
+      <p className="subtitle">Software Engineer</p>
       <p>
         Currently working on a website and a mobile app. I have learned React, JS, SQL, Java, Springboot, REST API's, 
         and refining my front end and back end capabilities.
